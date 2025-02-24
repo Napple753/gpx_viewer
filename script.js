b826fca0605d65580f26758c42917cad7b63863f
Napple753 <napple.com@gmail.com>
2025-02-24 11:22:44 +0900

    スライダー追加

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Initialize the map
 const map = L.map('map').setView([0, 0], 2);
 
+// Get the time slider element
+const timeSlider = document.getElementById('time-slider');
+
 // Add OpenStreetMap tiles
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -11,34 +14,39 @@ fetch('output/points_data.json')
     .then(response => response.json())
     .then(data => {
         const points = data.points;
-        console.log(points);
+
+        // Set the min and max values for the time slider
+        timeSlider.min = new Date(points[0].time).getTime();
+        timeSlider.max = new Date(points[points.length - 1].time).getTime();
+
         let index = 0;
 
-            // Create a marker and add it to the map
-            const marker = L.marker([0, 0]).addTo(map);
+        // Create a marker and add it to the map
+        const marker = L.marker([0, 0]).addTo(map);
 
-            // Create a polyline and add it to the map
-            const polyline = L.polyline([], { color: 'lightblue' }).addTo(map);
-            const traveledPath = L.polyline([], { color: 'blue' }).addTo(map);
+        // Create a polyline and add it to the map
+        const polyline = L.polyline([], { color: 'lightblue' }).addTo(map);
+        const traveledPath = L.polyline([], { color: 'blue' }).addTo(map);
 
-            // Add all points to the polyline
-            points.forEach(point => {
-                polyline.addLatLng([point.lat, point.lng]);
-            });
-            // Fit the map view to the polyline
-            map.fitBounds(polyline.getBounds());
+        // Add all points to the polyline
+        points.forEach(point => {
+            polyline.addLatLng([point.lat, point.lng]);
+        });
+        // Fit the map view to the polyline
+        map.fitBounds(polyline.getBounds());
 
-            // Function to move the marker to a new point
-            function moveMarker() {
-                if (index < points.length) {
-                    const point = points[index];
-                    marker.setLatLng([point.lat, point.lng]);
-                    traveledPath.addLatLng([point.lat, point.lng]);
-                    
-                    index++;
-                    setTimeout(moveMarker, 100); // Adjust the interval as needed
-                }
+        // Function to move the marker to a new point
+        function moveMarker() {
+            if (index < points.length) {
+                const point = points[index];
+                marker.setLatLng([point.lat, point.lng]);
+                traveledPath.addLatLng([point.lat, point.lng]);
+                timeSlider.value = new Date(point.time).getTime();
+                
+                index++;
+                setTimeout(moveMarker, 100); // Adjust the interval as needed
             }
+        }
 
         // Start the animation
         moveMarker();
